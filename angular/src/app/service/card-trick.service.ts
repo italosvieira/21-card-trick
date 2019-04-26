4076b79a9b0234913094628e81c1ffe7b7f2e31f
italosvieira <italo.vieira.dev@gmail.com>
2019-04-26 17:35:04 -0300

    Changing service to log errors.

--- angular/src/app/service/card-trick.service.ts
+++ angular/src/app/service/card-trick.service.ts
@@ -13,13 +13,13 @@ export class CardTrickService {
     try {
       shuffle = await this.http.get<Shuffle>('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1').toPromise();
     } catch (e) {
-      throw new Exception('Error fetching new shuffled deck of cards from API.', e);
+      console.log('Error fetching new shuffled deck of cards from API.', e);
     }
 
     try {
       return await this.http.get<Deck>(`https://deckofcardsapi.com/api/deck/${shuffle.deck_id}/draw/?count=21`).toPromise();
     } catch (e) {
-      throw new Exception('Error fetching new deck with 21 cards from API.', e);
+      console.log('Error fetching new deck with 21 cards from API.', e);
     }
   }
 
