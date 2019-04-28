68b6e6be27a28c898366899eb55ab53503ac0451
italosvieira <italo.vieira.dev@gmail.com>
2019-04-27 21:13:01 -0300

    Starting tests with vue.

--- vue/tests/unit/example.spec.ts
+++ vue/tests/unit/example.spec.ts
@@ -1,12 +1,8 @@
-import { shallowMount } from '@vue/test-utils';
-import HelloWorld from '@/component/CardTrick.vue';
+import {mount} from '@vue/test-utils';
+import CardTrick from '@/component/CardTrick.vue';
 
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message';
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg },
-    });
-    expect(wrapper.text()).toMatch(msg);
+describe('CardTrick.vue', () => {
+  test('Is a Vue instance', () => {
+    expect(mount(CardTrick).isVueInstance()).toBeTruthy();
   });
 });
